8544947b26c303144082159a1eef052e7cb17dc4
jan altisent <altisentbrescojan@alumnes.ilerna.com>
2026-01-13 16:21:40 +0100

    classe fil

---
+++ src/vectors_3/vectors_3.java
@@ -0,0 +1,24 @@
+package vectors_3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class vectors_3 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        if () {
+
+        } else {
+
+        }
+
+        System.out.println("Valor màxim: " +  "Fil: " + );
+
+
+
+    }
+}
