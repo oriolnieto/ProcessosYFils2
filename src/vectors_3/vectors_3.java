1bdc5421676850cb0684e1b89a1580ced8ce220f
jan altisent <altisentbrescojan@alumnes.ilerna.com>
2026-01-19 20:16:33 +0100

    Update vectors_3.java

--- src/vectors_3/vectors_3.java
+++ src/vectors_3/vectors_3.java
@@ -1,5 +1,6 @@
 package vectors_3;
 
+import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,6 +10,33 @@ public class vectors_3 {
 
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
+
+        System.out.println("Introdueix el valor del vector: ");
+        int n = sc.nextInt();
+
+        System.out.println("Introdueix el nombre de fills: ");
+        int fills = sc.nextInt();
+
+        int [] vector = new int[n];
+        for (int i=0; i<n; i++){
+            vector[i] = rand.nextInt(1000);
+        }
+
+        int mida = n / fills;
+        int index = 0;
+
+        FilVector[] fils = new FilVector[fills];
+
+        for (int i=0; i<fills; i++){
+            int inici = index;
+            int fi = index + fills;
+            fils[i] = new FilVector(vector , index , fi , inici);
+            fils[i].start();
+            index = fi;
+        }
+
+        int 
+
 
         if () {
 
