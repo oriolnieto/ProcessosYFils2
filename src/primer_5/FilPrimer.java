a0c738b8f8daab9a71dc38b8d76289f27c709402
Oriol Nieto <oriolnieto2006@gmail.com>
2026-01-26 19:54:42 +0100

    Commit Intermig #5

--- src/primer_5/FilPrimer.java
+++ src/primer_5/FilPrimer.java
@@ -20,15 +20,14 @@ public class FilPrimer implements Runnable {
     @Override
     public void run() {
 
-        for (int contador = 0; contador < numero; contador++) {
-            if (numero == 0) {
-                resultat = 0;
-            } else {
-                c = a + b; // la suma de update sera el primer más el segundo en fibonacci
-                a = b; // igualamos el valor que habia en b para a
-                b = c; // igualamos el valor de c en b, que sera la suma que hemos echo previamente, y asi sucesivamente
-            }
-            resultat = a;
+        if (numero%2==0) {
+            return false;
         }
+
+        for(int i=3;i*i<=n;i+=2) {
+            if(n%i==0)
+                return false;
+        }
+            return true;
     }
 }
--- src/primer_5/primers_5.java
+++ src/primer_5/primers_5.java
@@ -1,4 +1,41 @@
 package primer_5;
 
+import primer_5.FilPrimer;
+
+import java.util.Scanner;
+
 public class primers_5 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num = 0;
+        int contador = 1;
+
+        while (num != -1) {
+            System.out.println("Escriu un numero:");
+             num = scan.nextInt();
+             contador++;
+        }
+
+        Thread[] fils = new Thread[contador];
+        FilPrimer[] filPrimers = new FilPrimer[contador];
+
+        for (int i = 0; i < contador; i++) {
+            filPrimers[i] = new FilPrimer(i); // guardar en array
+            fils[i] = new Thread(filPrimers[i]);
+            fils[i].start();
+        }
+
+        for (int i = 0; i < contador; i++) {
+            try {
+                fils[i].join(); // s'ha de fer el join apart perque si n'ho es fa tot sincronitzat es a dir, es fa de forma secuencial, ademés de que ho necessitem per a que acabi cada fil el seu calcul de fibonacci i no peti
+            } catch (InterruptedException e) {
+                System.out.println("Error fent el Join!");
+            }
+        }
+
+        for (int i = 0; i < contador; i++) {
+            System.out.println(filPrimers[i].getResultat());
+        }
+    }
 }
