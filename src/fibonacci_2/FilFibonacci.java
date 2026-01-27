843ad5f508725e543f08fa301ca51c1bbd79b8e6
Oriol Nieto <oriolnieto2006@gmail.com>
2026-01-27 15:46:59 +0100

    eliminat innecesari

--- src/fibonacci_2/FilFibonacci.java
+++ src/fibonacci_2/FilFibonacci.java
@@ -24,14 +24,9 @@ public class FilFibonacci implements  Runnable {
         int c = 0;
 
         for (int contador = 0; contador < numero; contador++) {
-            if (numero == 0){
-                resultat = 0;
-            }
-            else {
                 c = a + b; // la suma de update sera el primer más el segundo en fibonacci
                 a = b; // igualamos el valor que habia en b para a
                 b = c; // igualamos el valor de c en b, que sera la suma que hemos echo previamente, y asi sucesivamente
-            }
             resultat = a;
         }
     }
